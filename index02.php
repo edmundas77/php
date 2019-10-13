9a0a1627ebd7b38d390b43a65db83d1712b85481
edmundas77 <edmundas77@gmail.com>
2019-10-13 22:32:38 +0300

    Pridėta užduotis php02

---
+++ index02.php
@@ -0,0 +1,8 @@
+<?php
+        $country = 'Lietuva';
+        $city = 'Vilnius';
+        $population = 574221;
+?>
+
+<p><?php echo $city . ' - Lietuvos sostinė.'; ?></p>
+<p><?php echo "$city ir $country sparčiai auga." ; ?></p>
